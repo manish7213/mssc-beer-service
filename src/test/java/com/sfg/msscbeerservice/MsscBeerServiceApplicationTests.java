84fe07120079fc0fe9e70920a0c3af25afce2f07
manish7213 <manish.1si11cs057@gmail.com>
2020-09-04 20:23:56 +0530

    Initial commit

---
+++ src/test/java/com/sfg/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
