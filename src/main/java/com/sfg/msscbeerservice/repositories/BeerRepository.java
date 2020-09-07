e2b8ae160926eec151e18f88fd5fc4908a981402
manish7213 <manish.1si11cs057@gmail.com>
2020-09-07 14:40:42 +0530

    Added jpa repository

---
+++ src/main/java/com/sfg/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.sfg.msscbeerservice.repositories;
+
+import com.sfg.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+    
+}
